afb2b5c3ebf67124020de361f16dca341bd37def
Senthil Maruthaiappan <senips@gmail.com>
2015-09-05 15:32:15 -0400

    rename dir

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,7 +20,7 @@ module.exports = function (grunt) {
 
   // Configurable paths for the application
   var appConfig = {
-    app: require('./bower.json').appPath || 'www',
+    app: require('./bower.json').appPath || 'app',
     dist: 'dist'
   };
 
